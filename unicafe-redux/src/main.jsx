33d63a10e5b443171947ce536f2f4e604c893f18
Learner Getready <learnergit@hymyilee.fi>
2023-11-27 13:52:38 +0200

    6.2

---
+++ unicafe-redux/src/main.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+const App = () => {
+  
+  const dispatch = (what) => {
+    store.dispatch({
+      type: what
+    })
+  }
+
+  const votes = store.getState()
+
+  return (
+    <div>
+      <button onClick={() => dispatch('GOOD')}>good</button> 
+      <button onClick={() => dispatch('OK')}>ok</button> 
+      <button onClick={() => dispatch('BAD')}>bad</button>
+      <button onClick={() => dispatch('ZERO')}>reset stats</button>
+      <div>good {votes.good}</div>
+      <div>ok {votes.ok}</div>
+      <div >bad {votes.bad}</div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
