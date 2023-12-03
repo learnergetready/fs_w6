4f5dd48bbfe7385886b1e2341592fe1e9400dd32
Learner Getready <learnergit@hymyilee.fi>
2023-12-03 17:05:41 +0200

    6.16

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
@@ -10,11 +11,22 @@ const anecdoteSlice = createSlice({
     voteAnecdote(state, action) {
       return state.map(obj => obj.id !== action.payload ? obj : { ...obj, votes: obj.votes +1 })
     },
+    appendAnecdotes(state, action) {
+      state.push(action.payload)
+    },
     setAnecdotes(state, action) {
       return action.payload
     }
   }
 })
 
-export const { addAnecdote, voteAnecdote, setAnecdotes } = anecdoteSlice.actions
+export const { addAnecdote, voteAnecdote, appendAnecdotes, setAnecdotes } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
 export default anecdoteSlice.reducer
